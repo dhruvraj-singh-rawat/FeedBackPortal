027223017825936716cc8e31063a64f400d510d8
Dhruvraj Rawat <dhruvraj-singh-rawat@users.noreply.github.com>
2017-08-27 14:43:59 +0530

    Updated Testing Database

--- testing database/master.sql
+++ testing database/master.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 27, 2017 at 08:39 AM
+-- Generation Time: Aug 27, 2017 at 11:13 AM
 -- Server version: 10.1.25-MariaDB
 -- PHP Version: 5.6.31
 
@@ -55,6 +55,13 @@ CREATE TABLE `courses` (
   `faculty` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `courses`
+--
+
+INSERT INTO `courses` (`id`, `course_name`, `year`, `faculty`) VALUES
+(1, 'ECE-1', 1, 'Sandeep Saini');
+
 -- --------------------------------------------------------
 
 --
@@ -64,16 +71,19 @@ CREATE TABLE `courses` (
 CREATE TABLE `faculty` (
   `id` int(11) NOT NULL,
   `name` varchar(50) NOT NULL,
-  `year` int(10) NOT NULL
+  `year` int(10) NOT NULL,
+  `department` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `faculty`
 --
 
-INSERT INTO `faculty` (`id`, `name`, `year`) VALUES
-(1, 'Sandeep Saini ', 1),
-(2, 'Sunil Kumar Gautam', 1);
+INSERT INTO `faculty` (`id`, `name`, `year`, `department`) VALUES
+(1, 'Sandeep Saini ', 1, '3'),
+(2, 'Sunil Kumar Gautam', 1, '5'),
+(3, 'Kapil Jainwal', 2, '3'),
+(4, 'Deepak Nair', 3, '3');
 
 -- --------------------------------------------------------
 
@@ -110,12 +120,12 @@ ALTER TABLE `faculty`
 -- AUTO_INCREMENT for table `courses`
 --
 ALTER TABLE `courses`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 --
 -- AUTO_INCREMENT for table `faculty`
 --
 ALTER TABLE `faculty`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;COMMIT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
